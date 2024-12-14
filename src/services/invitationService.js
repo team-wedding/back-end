4613212e81c950ea6d074da8ff93b2da1d2ba611
znhxxn <ghoon7777@naver.com>
2024-12-14 17:54:46 +0900

    chore: 청첩장 관련 구조 생성

---
+++ src/services/invitationService.js
@@ -0,0 +1,30 @@
+const invitatonRepository = require('../repositories/invitationRepository')
+const authUtil = require('../utils/authUtil')
+
+const postInvitation = async () => {
+
+};
+
+const getInvitation = async () => {
+
+};
+
+const putInvitation = async () => {
+
+};
+
+const deleteInvitation = async () => {
+
+};
+
+const getInvitations = async () => {
+
+};
+
+module.exports = {
+    postInvitation,
+    getInvitation,
+    putInvitation,
+    deleteInvitation,
+    getInvitations
+};
