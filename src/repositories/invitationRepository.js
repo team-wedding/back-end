9869a751dcdd22bb372804b89d519a7fe20c3b67
znhxxn <ghoon7777@naver.com>
2024-12-16 12:57:05 +0900

    feat: invitation 기능 구현

--- src/repositories/invitationRepository.js
+++ src/repositories/invitationRepository.js
@@ -1,30 +1,83 @@
 const { Invitation } = require('../models');
 
-const createInvitation = async () => {
-
+const createInvitation = async (invitationData) => {
+  try {
+    const newInvitation = await Invitation.create({
+      userId: invitationData.userId,
+      groomName: invitationData.groomName,
+      brideName: invitationData.brideName,
+      groomContact: invitationData.groomContact,
+      brideContact: invitationData.brideContact,
+      date: invitationData.date,
+      location: invitationData.location,
+      imgUrl: invitationData.imgUrl,
+      contentType: invitationData.contentType,
+      content: invitationData.content,
+      groomFatherName: invitationData.groomFatherName,
+      groomMotherName: invitationData.groomMotherName,
+      brideFatherName: invitationData.brideFatherName,
+      brideMotherName: invitationData.brideMotherName,
+      groomFatherContact: invitationData.groomFatherContact,
+      groomMotherContact: invitationData.groomMotherContact,
+      brideFatherContact: invitationData.brideFatherContact,
+      brideMotherContact: invitationData.brideMotherContact,
+    });
+    return newInvitation;
+  } catch (err) {
+    throw new Error('invitationRepository createInvitation err', err);
+  }
 };
 
-const getInvitationById = async () => {
-
+const getInvitationById = async (invitationId) => {
+  try {
+    const invitation = await Invitation.findOne({
+      where: { id: invitationId },
+      raw: true,
+    });
+    return invitation;
+  } catch (err) {
+    throw new Error('invitationRepository getInvitationById err', err);
+  }
 };
 
-const getInvitationsByUserId = async () => {
-
+const getInvitationsByUserId = async (userId) => {
+  try {
+    const invitations = await Invitation.findAll({
+      where: { userId: userId },
+      raw: true,
+    });
+    return invitations;
+  } catch (err) {
+    throw new Error('invitationRepository getInvitationsByUserId err', err);
+  }
 };
 
-const updateInvitation = async () => {
-
+const updateInvitation = async (invitationId, updatedData) => {
+  try {
+    await Invitation.update(updatedData, {
+      where: { id: invitationId },
+    });
+    return true;
+  } catch (err) {
+    throw new Error('invitationRepository updateInvitation err', err);
+  }
 };
 
-const deleteInvitation = async () => {
-    
+const deleteInvitation = async (invitationId) => {
+  try {
+    await Invitation.destroy({
+      where: { id: invitationId },
+    });
+    return true;
+  } catch (err) {
+    throw new Error('invitationRepository deleteInvitation err', err);
+  }
 };
 
 module.exports = {
-    createInvitation,
-    getInvitationById,
-    getInvitationsByUserId,
-    updateInvitation,
-    deleteInvitation,
-  };
-
+  createInvitation,
+  getInvitationById,
+  getInvitationsByUserId,
+  updateInvitation,
+  deleteInvitation,
+};
--- src/services/invitationService.js
+++ src/services/invitationService.js
@@ -1,24 +1,60 @@
-const invitatonRepository = require('../repositories/invitationRepository')
-const authUtil = require('../utils/authUtil')
+const invitationRepository = require('../repositories/invitationRepository');
+const authUtil = require('../utils/authUtil');
 
-const postInvitation = async () => {
-
+const postInvitation = async (userId, invitationData) => {
+    try {
+        const newInvitation = await invitationRepository.createInvitation(invitationData);
+        return { id: newInvitation.id };
+    } catch (err) {
+        throw new Error('invitationService postInvitation Err', err);
+    }
 };
 
-const getInvitation = async () => {
-
+const getInvitation = async (invitationId) => {
+    try {
+        const invitation = await invitationRepository.getInvitationById(invitationId);
+        if (!invitation) {
+            return null;
+        }
+        return invitation;
+    } catch (err) {
+        throw new Error('invitationService getInvitation Err', err);
+    }
 };
 
-const putInvitation = async () => {
-
+const putInvitation = async (userId, invitationId, updatedData) => {
+    try {
+        const invitation = await invitationRepository.getInvitationById(invitationId);
+        if (!invitation || invitation.userId !== userId) {
+            return false;
+        }
+        const isUpdated = await invitationRepository.updateInvitation(invitationId, updatedData);
+        return isUpdated;
+    } catch (err) {
+        throw new Error('invitationService putInvitation Err', err);
+    }
 };
 
-const deleteInvitation = async () => {
-
+const deleteInvitation = async (userId, invitationId) => {
+    try {
+        const invitation = await invitationRepository.getInvitationById(invitationId);
+        if (!invitation || invitation.userId !== userId) {
+            return false;
+        }
+        const isDeleted = await invitationRepository.deleteInvitation(invitationId);
+        return isDeleted;
+    } catch (err) {
+        throw new Error('invitationService deleteInvitation Err', err);
+    }
 };
 
-const getInvitations = async () => {
-
+const getInvitations = async (userId) => {
+    try {
+        const invitations = await invitationRepository.getInvitationsByUserId(userId);
+        return invitations || [];
+    } catch (err) {
+        throw new Error('invitationService getInvitations Err', err);
+    }
 };
 
 module.exports = {
@@ -26,5 +62,5 @@ module.exports = {
     getInvitation,
     putInvitation,
     deleteInvitation,
-    getInvitations
+    getInvitations,
 };
