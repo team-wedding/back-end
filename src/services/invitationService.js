4e40ecd4b59fe13f3e72894a387608fc5f9e7d25
znhxxn <ghoon7777@naver.com>
2025-01-07 21:08:48 +0900

    fix: invitation 수정, 삭제 수정

--- src/services/invitationService.js
+++ src/services/invitationService.js
@@ -5,7 +5,7 @@ const createInvitation = async (userId, invitationData) => {
         const newInvitation = await invitationRepository.createInvitation({...invitationData, userId});
         return { id: newInvitation.id };
     } catch (err) {
-        throw new Error('invitationService postInvitation Err', err);
+        throw new Error('청첩장 등록 에러', err);
     }
 };
 
@@ -17,33 +17,45 @@ const getInvitationById = async (invitationId) => {
         }
         return invitation;
     } catch (err) {
-        throw new Error('invitationService getInvitation Err', err);
+        throw new Error('청첩장 조회 에러', err);
     }
 };
 
 const updateInvitation = async (userId, invitationId, updatedData) => {
     try {
         const invitation = await invitationRepository.getInvitationById(invitationId);
-        if (!invitation || invitation.userId !== userId) {
-            return false;
+        if (!invitation) {
+            throw new Error('해당 청첩장이 없습니다');
         }
+
+        if (invitation.userId !== userId) {
+            throw new Error('토큰이 없어 수정할 수 없습니다');
+        }
+
         const isUpdated = await invitationRepository.updateInvitation(invitationId, updatedData);
+
         return isUpdated;
     } catch (err) {
-        throw new Error('invitationService putInvitation Err', err);
+        console.error('청첩장 수정 에러:', err.message);
+        throw new Error('청첩장 수정 에러');
     }
 };
 
 const deleteInvitation = async (userId, invitationId) => {
     try {
         const invitation = await invitationRepository.getInvitationById(invitationId);
-        if (!invitation || invitation.userId !== userId) {
-            return false;
+        if (!invitation) {
+            throw new Error('해당 청첩장이 없습니다');
         }
+
+        if (invitation.userId !== userId) {
+            throw new Error('토큰이 없어 수정할 수 없습니다');
+        }
+
         const isDeleted = await invitationRepository.deleteInvitation(invitationId);
         return isDeleted;
     } catch (err) {
-        throw new Error('invitationService deleteInvitation Err', err);
+        throw new Error('청첩장 삭제 에러', err);
     }
 };
 
@@ -52,7 +64,7 @@ const getInvitationsByUserId = async (userId) => {
         const invitations = await invitationRepository.getInvitationsByUserId(userId);
         return invitations || [];
     } catch (err) {
-        throw new Error('invitationService getInvitations Err', err);
+        throw new Error('청첩장 조회 에러', err);
     }
 };
 
