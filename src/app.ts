7600f6a45ae609c7aa146ec14fb9492835ced7df
Heebeom Song <songh6508@naver.com>
2025-02-03 15:31:52 +0900

    Fix: 임시 비밀번호 규치 수정, 테이블 관계 추가(user, token)

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:3000'], crede
 
 // sequelize db 연결
 sequelize
-  .sync({ alter: true })
+  .sync({alter : true})
   .then(() => {
     console.log("데이터베이스 연결 및 최신화 성공! ");
   })
